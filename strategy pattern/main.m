1341d8d425577b1ffb8b70fe1c28e6b9ac0e6405
gaoweiwei <getfirstblood@gmail.com>
2012-11-28 10:31:28 +0800

    Initial Commit

---
+++ strategy pattern/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  strategy pattern
+//
+//  Created by gaoweiwei on 12-11-28.
+//  Copyright (c) 2012年 gaoweiwei. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
